da0835fd06db82c9b3a69cd13b07e5848a1461f1
shermondevelopment <victorshermon@yahoo.com>
2022-07-12 21:07:59 -0300

    chore: including library of cors in project

--- app/server.ts
+++ app/server.ts
@@ -1,11 +1,18 @@
 /** dotenv - variables */
 import 'dotenv/config'
 
+/** cors */
+import cors from 'cors'
+
 /** express */
 import express, { json } from 'express'
 
 /** initializing server */
 const app = express()
+
+/** middleware setting server */
+app.use(json())
+app.use(cors())
 
 /** running server in port 5000 */
 app.listen(process.env.PORT || 5000, () => console.log(`app running in ${process.env.PORT} 🚀🚀🚀🚀🚀`))
